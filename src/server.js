fbcb37a9d235dad1ae49330eef3bb05d7576026d
al3464 <al3464@g.rit.edu>
2026-02-18 18:04:14 +0800

    add some code

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const cssFileHandler = require('./cssResponses.js')
+
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+    console.log(request.url);
+
+    switch(request.url){
+        case '/':
+            htmlHandler.getIndex(request,response);
+            break;
+
+        case '/style.css':
+            cssFileHandler.getCssFile(request, response);
+            break;
+        default:
+            htmlHandler.getIndex(request, response);
+            break;
+    }
+}
+
+
+
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Listening on 127.0.0.1 : ${port}`);
+});
